3ea96bf32af3b6cf5d7795db77c883340a8ca849
Aybek Amanbaev <idevkun@gmail.com>
2021-09-30 20:42:46 +0500

    added rightside-menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function () {
-
-})
+  $(".header__btn").on("click", function () {
+    $(".rightside-menu").removeClass("rightside-menu--close");
+  });
+  $(".rightside-menu__close").on("click", function () {
+    $(".rightside-menu").addClass("rightside-menu--close");
+  });
+  
+ /*  $("body").on("click", function () {
+    if(!($(".rightside-menu").hasClass("rightside-menu--close"))){
+     console.log("gello");
+     $(".rightside-menu").addClass("rightside-menu--close");
+    }
+  }); */
+});
